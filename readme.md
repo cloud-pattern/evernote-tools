9cd03d73b3dd4cfe2b24cae7a643d61a4077817f
reflorance <reflorance@gmail.com>
2015-09-02 03:14:39 -0700

    it pretty much works some of the time

---
+++ readme.md
@@ -0,0 +1,16 @@
+
+little demo for if you import notes to evernote from a different note program and your note titles were messed up.
+
+this will replace the title with the first line of the body of the note,
+    then append the current title to the end of your note, 
+
+for the first arg, give it a "p" for production, anything else for sandbox
+       2nd and 3rd optional:
+        , second arg is the notebook name
+        ,third is a tag name
+
+it shows you how to do both oauth or just using your dev token
+    the oauth is kinda crazy cause you have to paste it into your browser for user approval
+
+
+
--- renamer.py
+++ renamer.py
@@ -4,6 +4,7 @@ import sys
 import evernote
 from evernote.api.client import EvernoteClient
 import evernote.edam.notestore.NoteStore as NoteStore
+from evernote.edam.type.ttypes import NoteSortOrder
 from BeautifulSoup import NavigableString
 from BeautifulSoup import BeautifulSoup as BS
 
@@ -29,8 +30,7 @@ class Renamer:
         auth_token = client.get_access_token(
             request_token['oauth_token'],
             request_token['oauth_token_secret'],
-            vals['oauth_verifier']
-        )
+            vals['oauth_verifier'])
         return client
 
     def get_client_dev(self):
@@ -40,14 +40,18 @@ class Renamer:
     def get_notestore(self, client):
         return client.get_note_store()
 
-    def get_notebook(self, notestore, index):
-        return notestore.listNotebooks()[index]
+    def get_notebook(self, notestore, guid):
+        for notebook in notestore.listNotebooks():
+            if notebook.guid == guid:
+                return notebook
 
-
-    def get_notemeta_list(self, notestore, notebook, offset, limit):
-        filter = evernote.edam.notestore.ttypes.NoteFilter()
+    def get_notemeta_list(self, notestore, notebook, tag, offset, limit):
+        filter = evernote.edam.notestore.ttypes.NoteFilter(order=NoteSortOrder.UPDATED)
         filter.notebookGuid = notebook.guid
-        spec = NoteStore.NotesMetadataResultSpec()
+        if tag != None:
+            filter.tagGuids = [tag.guid]
+        spec = NoteStore.NotesMetadataResultSpec(includeTitle = True,
+                                                 includeTagGuids = True)
 
         meta = notestore.findNotesMetadata(self.token, filter, offset, limit, spec)
         return meta.notes
@@ -78,6 +82,21 @@ class Renamer:
         note.content = str(notesoup)
         note = notestore.updateNote(note)
 
+    def get_note_tags(self, note):
+        return 0
+
+    def get_tag_by_name(self, notestore, tag_name):
+        tag_list = notestore.listTags()
+        for tag in tag_list:
+            if tag.name == tag_name:
+                return tag
+    
+    def get_notebook_by_name(self, notestore, notebook_name):
+        notebook_list = notestore.listNotebooks()
+        for notebook in notebook_list:
+            if notebook.name== notebook_name:
+                return notebook
+
     @staticmethod
     def parse_query_string(authorize_url):
         uargs = authorize_url.split('?')
@@ -104,18 +123,26 @@ class Renamer:
 if __name__ == '__main__':
 
     keys_filepath = "keys.txt"
+    old_title_charcount = 38
     is_sandbox = True
-    notebook_index = 0
+    notebook_name = "First Notebook"
+    tag_name = None 
     offset = 0
     limit = 10
 
     keys = Renamer.get_keys(keys_filepath)
 
-    if sys.argv[1] == "p":
+    if len(sys.argv) > 1 and sys.argv[1] == "p":
         keys["token"] = keys["prod_token"]
         keys["url"] = keys["prod_url"]
         is_sandbox = False
-        print "running on production"
+        print "***running on production***"
+
+    if len(sys.argv) > 2:
+        notebook_name = sys.argv[2]
+
+    if len(sys.argv) > 3:
+        tag_name = sys.argv[3]
         
     rn = Renamer(keys["customer_key"],
                  keys["customer_secret"],
@@ -125,19 +152,48 @@ if __name__ == '__main__':
 
     client = rn.get_client_dev()
     notestore = rn.get_notestore(client)
-    notebook = rn.get_notebook(notestore, notebook_index)
-    noteslist = rn.get_notemeta_list(notestore, notebook, offset, limit)
 
-    n0 = rn.get_note(noteslist[0], notestore)
-    n0s = BS(n0.content)
-    n0f = n0s.findAll('en-note')[0]
-    n1 = rn.get_note(noteslist[1], notestore)
-    n1s = BS(n1.content)
-    n1f = n1s.findAll('en-note')[0]
-    n2 = rn.get_note(noteslist[2], notestore)
-    n2s = BS(n0.content)
-    n2f = n2s.findAll('en-note')[0]
-    retrieved_notes = [n0, n1, n2]
+    notebook = rn.get_notebook_by_name(notestore, notebook_name)
+    if notebook == None:
+        print "no such notebook"
+
+    tag = None
+    if tag_name != None: 
+        found_tag = rn.get_tag_by_name(notestore, tag_name)
+        if found_tag != None:
+            tag = found_tag
+        else:   
+            print "no such tag, ignored"
+
+    noteslist = rn.get_notemeta_list(notestore, notebook, tag, offset, limit)
+    print "there were %s rename candidates" % len(noteslist)
+
+    while len(noteslist) > 0:
+        for note_meta in noteslist:
+            note = rn.get_note(note_meta, notestore)
+            old_title = rn.get_note_title(note)
+            if len(old_title) == old_title_charcount:
+                print "matching title found: %s" % old_title
+                new_title = rn.get_note_firstline(note)
+                rn.update_title(note, notestore, new_title)
+                print "title updated to: %s" % new_title
+                rn.append_body(note, notestore, old_title)
+                print "appended old title to body"
+        offset += limit
+        noteslist = rn.get_notemeta_list(notestore, notebook, tag, offset, limit)
+        
+
+   # ipython crap
+   # n0 = rn.get_note(noteslist[0], notestore)
+   # n0s = BS(n0.content)
+   # n0f = n0s.findAll('en-note')[0]
+   # n1 = rn.get_note(noteslist[1], notestore)
+   # n1s = BS(n1.content)
+   # n1f = n1s.findAll('en-note')[0]
+   # n2 = rn.get_note(noteslist[2], notestore)
+   # n2s = BS(n0.content)
+   # n2f = n2s.findAll('en-note')[0]
+   # retrieved_notes = [n0, n1, n2]
     
     
     
